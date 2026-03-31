558dd85137867a78d66ac304e4be32195833ab4f
gillsimran901-netizen <gillsimran901@gmail.com>
2026-03-31 21:36:02 +0530

    Add files via upload

---
+++ 13c.py
@@ -0,0 +1,19 @@
+import mysql.connector 
+
+
+
+
+
+mydb=mysql.connector.connect(host="localhost",username="root",password="",database="bscit")
+print(mydb)
+
+mycursor = mydb.cursor()
+sql="insert into record(name,email,age,mobile) values(%s,%s,%s,%s)"
+val=[('sanju','sanju@gmail.com','23','9815591802'),('rani','rani@yahoo.com','12','7784818000')]
+mycursor.executemany(sql,val)
+mydb.commit()
+print(mycursor.rowcount,"record was inserted")
+  
+mycursor.executemany("delete from students whose age>22")
+mydb.commit()
+print(mycursor.rowcount, "record was deleted")
